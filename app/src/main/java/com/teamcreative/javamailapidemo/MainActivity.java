8c5ab28a4189d8ac0dd1d00a753014e4457d81e3
Sergio <sergiosoler29@gmail.com>
2021-01-27 13:05:48 -0500

    first commit

---
+++ app/src/main/java/com/teamcreative/javamailapidemo/MainActivity.java
@@ -0,0 +1,71 @@
+package com.teamcreative.javamailapidemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText mEmail;
+    public EditText mSubject;
+    public EditText mMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mEmail = (EditText)findViewById(R.id.mailID);
+        mMessage = (EditText)findViewById(R.id.messageID);
+        mSubject = (EditText)findViewById(R.id.subjectID);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail() {
+
+        String mail = mEmail.getText().toString().trim();
+        String message = mMessage.getText().toString();
+        String subject = mSubject.getText().toString().trim();
+
+
+        JavaMailAPI javaMailAPI = new JavaMailAPI(this,mail,subject,message);
+
+        javaMailAPI.execute();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/teamcreative/javamailapidemo/Utils.java
@@ -0,0 +1,8 @@
+package com.teamcreative.javamailapidemo;
+
+public class Utils {
+    //Correo creado para enviar mensajes desde Gmail
+    public static final String EMAIL = "javaMail.WSD@gmail.com";
+
+    public static final String PASSWORD = "Asd12345";
+}
